e337341075c6a37b11887d80a7e9d8124b7e1104
tobi <tobi@16125c8e-1612-0410-aae7-9a83140a225b>
2008-01-31 18:53:47 +0000

    * Finalized setup  * Cleaned up HTML  * Fixed rendering of textinput tag  * Fixed rendering of stand-alone box  * Improved template rendering

--- branches/2.0/rss-box.js
+++ branches/2.0/rss-box.js
@@ -7,6 +7,7 @@ if (typeof org === "undefined") {
 }
 
 new function() {
+   var baseUri = "http://localhost/~tobi/rss-box/";
    var ref;
 
    ref = org.p3k = {};
@@ -19,24 +20,42 @@ new function() {
       titleBarColor: "lightblue",
       titleBarTextColor: "black",
       boxFillColor: "white",
-      textColor: "black"
+      textColor: "black",
+      showXmlButton: 1
    };
 
-   var pairs = location.search.substring(1).split("&");
-   var parts, value;
-   for (var i in pairs) {
-      parts = pairs[i].split("=");
-      value = decodeURIComponent(parts[1]);
-      value && (ref.param[decodeURIComponent(parts[0])] = value);
+   var query, url;
+
+   if (location.search) {
+      query = location.search.substr(1);
+   } else {
+      var scripts = document.getElementsByTagName("script");
+      for (var i=0; i<scripts.length; i+=1) {
+         url = scripts[i].src;
+         if (url.indexOf(baseUri) === 0) {
+            query = url.split("?")[1];
+            break;
+         }
+      }
    }
-   
+      
+   if (query) {
+      var pairs = query.split("&");
+      var parts, value;
+      for (var i in pairs) {
+         parts = pairs[i].split("=");
+         value = decodeURIComponent(parts[1]);
+         value && (ref.param[decodeURIComponent(parts[0])] = value);
+      }
+   }
+      
    var NAMESPACES = {
       dc: "http://purl.org/dc/elements/1.1/",
       rdf: "http://www.w3.org/1999/02/22-rdf-syntax-ns#"
    };
 
    var getUrl = function(url) {
-      url = "proxy.r?" + encodeURIComponent(url);
+      url = baseUri + "proxy.r?" + encodeURIComponent(url);
    
       var HttpRequest = function() {
          return (typeof XMLHttpRequest !== "undefined") ?
@@ -84,11 +103,78 @@ new function() {
       return str.replace(/^\s*(\S*)\s*$/, "$1");
    }
    
+   var render = function(template, param) {
+      for (var i in param) {
+         template = template.replace(new RegExp("\\$\\{" + i + "\\}", "g"), 
+               param[i] || "");
+      }
+      return template;
+   }
+   
+   var renderDate = function(str) {
+      var ISOPATTERN = /([0-9]{4})-([0-9]{2})-([0-9]{2})T([0-9:]+).*$/;
+
+      var padZero = function(n) {
+         if (n < 10) {
+            return "0" + n
+         }
+         return n;
+      }
+   
+      var date;
+      var millis = Date.parse(str.replace(ISOPATTERN, "$1/$2/$3 $4"));
+      if (millis) {
+         date = new Date(millis);
+      } else {
+         date = new Date;
+      }
+      return render(layout.date, {
+         year: date.getFullYear(),
+         month: padZero(date.getMonth() + 1),
+         day: padZero(date.getDate()),
+         hours: padZero(date.getHours()),
+         minutes: padZero(date.getMinutes()),
+         seconds: padZero(date.getSeconds())
+         //timeZone: date.getTimeZone()
+      });
+   }
+   
+   var renderButtons = function(enclosure, source) {
+      var result = "";
+      if (enclosure && enclosure.link) {
+         result += render(layout.image, {
+            source: baseUri + "enclosure.gif",
+            title: enclosure.type,
+            link: enclosure.link,
+            width: 16,
+            height: 16,
+            align: "",
+            hspace: "",
+            vspace: ""
+         });
+      }
+      if (source && source.link) {
+         result += render(layout.image, {
+            source: baseUri + "source.gif",
+            title: source.title,
+            link: source.link,
+            width: 15,
+            height: 15,
+            align: "",
+            hspace: "",
+            vspace: ""
+         });
+      }
+      return result;         
+   }
+      
    var param = org.p3k.param;
    var rss = org.p3k.rss = {items: []};
    
    var http = getUrl(param.url);
    var xml = http.responseXML;
+   var layout = eval(http.getResponseHeader("X-RssBox-Data"));
+      
    if (xml) {
       xml.normalize();
    
@@ -130,9 +216,12 @@ new function() {
       }
       
       if (type === "RDF") {
-         rss.date = getText(getNode(channel, "date", "dc"));
-         rss.rights = getText(getNode(channel, "rights", "dc"));
-         var input = getNode(xml, "textinput");
+         rss.date = renderDate(getText(getNode(channel, "date", "dc")));
+         rss.rights = getText(getNode(channel, "creator", "dc"));
+         var input = getNode(root, "textinput");
+         if (input && !getNode(input, "link")) {
+            input = root.getElementsByTagName("textinput")[1];
+         }
          if (input) {
             ref = rss.input = {};
             ref.link = getText(getNode(input, "link"));
@@ -141,7 +230,7 @@ new function() {
             ref.title = getText(getNode(input, "title"));
          }
       } else {
-         rss.date = getText(getNode(channel, "pubDate"));
+         rss.date = renderDate(getText(getNode(channel, "pubDate")));
          rss.rights = getText(getNode(channel, "copyright"));
       }
       
@@ -192,43 +281,6 @@ new function() {
         rss.items.push(ref);
       }
       
-      var render = function(template, param) {
-         for (var i in param) {
-            template = template.replace(new RegExp("\\$" + i, "g"), param[i] || "");
-         }
-         return template;
-      }
-      
-      var renderDate = function(str) {
-         var ISOPATTERN = /([0-9]{4})-([0-9]{2})-([0-9]{2})T([0-9:]+).*$/;
-   
-         var padZero = function(n) {
-            if (n < 10) {
-               return "0" + n
-            }
-            return n;
-         }
-      
-         var date;
-         var millis = Date.parse(str.replace(ISOPATTERN, "$1/$2/$3 $4"));
-         if (millis) {
-            date = new Date(millis);
-         } else {
-            date = new Date;
-         }
-         return render(layout.date, {
-            year: date.getFullYear(),
-            month: padZero(date.getMonth() + 1),
-            day: padZero(date.getDate()),
-            hours: padZero(date.getHours()),
-            minutes: padZero(date.getMinutes()),
-            seconds: padZero(date.getSeconds())
-            //timeZone: date.getTimeZone()
-         });
-      }
-      
-      var layout = eval(http.getResponseHeader("X-RssBox-Data"));
-      
       var item, items = "";
       for (var i in rss.items) {
          item = rss.items[i];
@@ -237,44 +289,48 @@ new function() {
             title: item.title,
             'break': item.title && item.description ? "<br />" : "",
             description: (!param.compact || !item.title) && item.description,
-            buttons: "" // enclosure + source
+            buttons: renderButtons(item.enclosure, item.source)
          });
       }
       
       var box = render(layout.box, {
-         boxTitle: rss.title,
+         title: rss.title,
          link: rss.link,
          description: rss.description,
+         items: items,
+
          xmlButton: param.showXmlButton && render(layout.image, {
             link: param.url,
             source: "http://p3k.org/rss/rss.png",
-            title: "",
+            title: rss.format + " " + rss.version,
             align: "right",
             width: "",
-            height: ""
+            height: "",
+            hspace: "",
+            vspace: ""
          }),
+         
          image: !param.compact && rss.image && render(layout.image, {
             link: rss.image.link,
             source: rss.image.source,
             width: rss.image.width,
             height: rss.image.height,
             title: rss.image.title,
-            
             align: "right",
             valign: "baseline",
             hspace: 5,
             vspace: 5
          }),
-         content: items,
-         textInput: !param.compact && rss.input && render(layout.input, {
+         
+         input: !param.compact && rss.input && render(layout.input, {
             link: rss.input.link,
             description: rss.input.description,
             name: rss.input.name,
             title: rss.input.title
          }),
-         lastBuildDate: renderDate(rss.date),
          
-         width: 200,
+         date: rss.date,
+         width: param.width,
          frameColor: param.frameColor,
          fontFace: param.fontFace,
          align: param.align,
@@ -286,5 +342,4 @@ new function() {
       
       document.write(box);
    }
-
-} ();
+}();
