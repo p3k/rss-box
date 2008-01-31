b6cbfc71340a2a2e64ee59b1b85035e503da5f17
tobi <tobi@16125c8e-1612-0410-aae7-9a83140a225b>
2008-01-31 23:17:20 +0000

    * Added caching for proxied RSS feeds  * Improved error handling (box shows the server message)  * Finalized template rendering (loop over available placeholders instead of values)  * Improved rendering when links / titles are missing  * Added referrer list (fetched via AJAX)

---
+++ branches/2.0/referrers.r
@@ -0,0 +1,23 @@
+#!/usr/local/bin/rebol -cs
+
+REBOL []
+
+do %json.r
+
+;print "Content-Type: text/plain^/"
+
+cache: %/tmp/rss-box/
+
+files: sort/compare read cache func [a b] [ 
+   (modified? join cache a) > modified? join cache b
+]
+
+list: make block! 50
+
+foreach file files [
+   append list to-string debase file
+   if (length? list) = 50 [break]
+]
+
+print "Content-Type: text/javascript^/"
+print to-json make object! [referrers: list]
