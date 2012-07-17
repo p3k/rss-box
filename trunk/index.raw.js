c5f6f2295e04fd5eb0b898f0f9e1b450cee056fd
tobi <tobi@16125c8e-1612-0410-aae7-9a83140a225b>
2012-07-17 23:22:58 +0000

    Modified service URLs to reflect move of AppEngine app to services.tobischaefer.com

--- trunk/index.raw.js
+++ trunk/index.raw.js
@@ -1,8 +1,8 @@
 (function() {
 
    var BASE_URI = location.protocol + '//p3k.org/rss/';
-   var ROXY_URI = location.protocol + '//p3k-001.appspot.com/roxy';
-   var FERRIS_URI = location.protocol + '//p3k-001.appspot.com/ferris?callback=?&group=rssbox';
+   var ROXY_URI = location.protocol + '//services.tobischaefer.com/roxy';
+   var FERRIS_URI = location.protocol + '//services.tobischaefer.com/ferris?callback=?&group=rssbox';
    
    if (typeof DEBUG !== 'undefined' && DEBUG === true) {
       BASE_URI = 'http://localhost/~tobi/rss-box/';
