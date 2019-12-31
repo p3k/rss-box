f1387777fbe1b5ec6f59fb35ce50d6d7edf8d0bd
Tobi Schäfer <interface@p3k.org>
2019-12-31 23:46:08 +0100

    chg: rebuilt dist files for production

--- dist/polyfill.io.js
+++ dist/polyfill.io.js
@@ -1,28 +1,2 @@
-this.polyfill = this.polyfill || {};
-this.polyfill.io = (function () {
-  'use strict';
-
-  function polyfill_io (callback) {
-
-    var features = [
-      'fetch',
-      'Object.assign',
-      'Promise',
-      'String.prototype.padStart',
-      'String.prototype.startsWith',
-      'Array.prototype.fill',
-      'Array.from'
-    ];
-
-    var script = document.createElement('script');
-    script.src = 'https://cdn.polyfill.io/v3/polyfill.min.js?flags=gated&features=' + encodeURIComponent(features.join());
-    script.crossOrigin = 'anonymous';
-    script.onload = callback;
-
-    document.head.appendChild(script);
-  }
-
-  return polyfill_io;
-
-}());
+this.polyfill=this.polyfill||{},this.polyfill.io=function(){"use strict";return function(t){var o=document.createElement("script");o.src="https://cdn.polyfill.io/v3/polyfill.min.js?flags=gated&features="+encodeURIComponent(["fetch","Object.assign","Promise","String.prototype.padStart","String.prototype.startsWith","Array.prototype.fill","Array.from"].join()),o.crossOrigin="anonymous",o.onload=t,document.head.appendChild(o)}}();
 //# sourceMappingURL=polyfill.io.js.map
