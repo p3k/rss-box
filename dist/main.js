43817d238f7a2e6b0c116ad02d59246ae228b554
Tobi Schäfer <interface@p3k.org>
2023-07-22 20:18:46 +0200

    Run npm-upgrade and rebuild dist files

--- dist/main.js
+++ dist/main.js
@@ -1,2 +1,57 @@
-!function(){"use strict";var r={app:"https://p3k.org/rss",proxy:"https://p3k.org/json/roxy",referrers:"https://p3k.org/json/ferris?group=rss-box",feed:"https://blog.p3k.org/stories.xml"},o="http://localhost",t={app:o+":8000",proxy:o+":8000/roxy",referrers:o+":8000/ferris?group=rss-box",feed:"https://blog.p3k.org/stories.xml"};Object.keys(r).forEach((function(o){o in t&&(t[o]=r[o])}));var e,s,p="__rssbox_viewer_"+"21.12.11".replace(/\D/g,"_")+"_init__";window[p]||(window[p]=!0,e=function(){var r=document.createElement("script");r.defer=r.async=!0,r.src=t.app+"/box.js",document.head.appendChild(r)},(s=document.createElement("script")).src="https://cdn.polyfill.io/v3/polyfill.min.js?flags=gated&features="+encodeURIComponent(["fetch","Object.assign","Promise","String.prototype.padStart","String.prototype.startsWith","Array.prototype.fill","Array.from"].join()),s.crossOrigin="anonymous",s.onload=e,document.head.appendChild(s))}();
+(function () {
+  'use strict';
+
+  const urls$1 = {};
+
+  const baseUrl = "http://localhost";
+
+  const urls = {
+    app: baseUrl + ":8000",
+    proxy: baseUrl + ":8000/roxy",
+    referrers: baseUrl + ":8000/ferris?group=rss-box",
+    feed: "https://blog.p3k.org/stories.xml"
+  };
+
+  Object.keys(urls$1).forEach(key => {
+    if (key in urls) urls[key] = urls$1[key];
+  });
+
+  var version = "21.12.11";
+
+  var polyfill = callback => {
+
+    const features = [
+      "fetch",
+      "Object.assign",
+      "Promise",
+      "String.prototype.padStart",
+      "String.prototype.startsWith",
+      "Array.prototype.fill",
+      "Array.from"
+    ];
+
+    const script = document.createElement("script");
+    script.src = "https://cdn.polyfill.io/v3/polyfill.min.js?flags=gated&features=" + encodeURIComponent(features.join());
+    script.crossOrigin = "anonymous";
+    script.onload = callback;
+
+    document.head.appendChild(script);
+  };
+
+  // This is the entrypoint for embedded box scripts
+
+  const id = `__rssbox_viewer_${version.replace(/\D/g, "_")}_init__`;
+
+  if (!window[id]) {
+    window[id] = true;
+
+    polyfill(() => {
+      const script = document.createElement("script");
+      script.defer = script.async = true;
+      script.src = urls.app + "/box.js";
+      document.head.appendChild(script);
+    });
+  }
+
+})();
 //# sourceMappingURL=main.js.map
