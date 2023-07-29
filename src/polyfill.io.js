956798b95c5dc16160760561485f94e8fdc58d58
Tobi Schäfer <interface@p3k.org>
2023-07-29 18:06:35 +0200

    Update polyfill.io URL
    Fix conditional loading and delint

--- src/polyfill.io.js
+++ src/polyfill.io.js
@@ -3,22 +3,24 @@ let loaded = false;
 export default callback => {
   if (loaded) return;
 
+  const polyfillUrl =
+    "https://cdn.polyfill.io/v3/polyfill.min.js?version=3.111.0&features=";
+
   const features = [
-    'fetch',
-    'Object.assign',
-    'Promise',
-    'String.prototype.padStart',
-    'String.prototype.startsWith',
-    'Array.prototype.fill',
-    'Array.from'
+    "fetch",
+    "Object.assign",
+    "Promise",
+    "String.prototype.padStart",
+    "String.prototype.startsWith",
+    "Array.prototype.fill",
+    "Array.from"
   ];
 
-  const script = document.createElement('script');
-  script.src =
-    'https://cdn.polyfill.io/v3/polyfill.min.js?flags=gated&features=' +
-    encodeURIComponent(features.join());
-  script.crossOrigin = 'anonymous';
+  const script = document.createElement("script");
+  script.src = `${polyfillUrl}${encodeURIComponent(features.join())}`;
+  script.crossOrigin = "anonymous";
   script.onload = callback;
-
   document.head.appendChild(script);
+
+  loaded = true;
 };
